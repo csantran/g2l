4664ef9ab170ad212cbf35d41ca0b67aaa525506
csantran <santrancedric@gmail.com>
2018-04-05 11:57:14 +0200

    Update, adding autosummary, napoleon and numpy doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,12 +29,17 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+napoleon_include_special_with_doc = True
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.pngmath',
     'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'numpydoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
