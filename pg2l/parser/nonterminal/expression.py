e526a4d37e254e668ed15bc66e1d6b2adc729e2c
csantran <santrancedric@gmail.com>
2018-04-08 12:58:56 +0200

    Add @staticmethod

--- pg2l/parser/nonterminal/expression.py
+++ pg2l/parser/nonterminal/expression.py
@@ -8,24 +8,27 @@ grammar.register('basenode', 'node', 'symbol', 'letter')()
 
 @register_with_nonterminal(G.node)
 class NodeParser(AbstractParserMixin):
-    
-    @docstring_production(G.basenode, G.node)    
-    def p_basenode_node(self, p):
+
+    @staticmethod
+    @docstring_production(G.basenode, G.node)
+    def p_basenode_node(p):
         """
         {0} : {1}
         """
         p[0] = p[1]
 
-    @docstring_production(G.node, G.letter)    
-    def p_node(self, p):
+    @staticmethod
+    @docstring_production(G.node, G.letter)
+    def p_node(p):
         """
         {0} : {1}
         """
         p[0] = ast.Node()
         p[0].push(p[1])
 
-    @docstring_production(G.letter, G.LETTER)    
-    def p_letter(self, p):
+    @staticmethod
+    @docstring_production(G.letter, G.LETTER)
+    def p_letter(p):
         """
         {0} : {1}
         """
@@ -36,23 +39,27 @@ grammar.register('expression', 'axiom', 'level')()
 @register_with_nonterminal(G.expression)
 class ExpressionParser(AbstractParserMixin):
 
-    @docstring_production(G.expression, G.axiom)    
-    def p_expression_axiom(self, p):
+    
+    @staticmethod
+    @docstring_production(G.expression, G.axiom)
+    def p_expression_axiom(p):
         """
         {0} : {1}
         """
         p[0] = p[1]
 
+    @staticmethod
     @docstring_production(G.axiom, G.level)
-    def p_axiom_level(self, p):
+    def p_axiom_level(p):
         """
         {0} : {1}
         """
         p[0] = ast.Axiom()
         p[0].push(p[1])
 
+    @staticmethod
     @docstring_production(G.level, G.basenode)
-    def p_level_basenode(self, p):
+    def p_level_basenode(p):
         """
         {0} : {1}
             | {0} {1}
@@ -60,7 +67,7 @@ class ExpressionParser(AbstractParserMixin):
         if len(p) == 2:
             p[0] = ast.Level()
             p[0].push(p[1])
-            
+
         elif len(p) == 3:
             p[0] = p[1]
             p[0].push(p[2])
