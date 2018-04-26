7ca46d3a11a6cd8bfff9ca987a2d3986cede9c94
csantran <santrancedric@gmail.com>
2018-04-26 12:17:00 +0200

    Cleaning code

--- pg2l/__init__.py
+++ pg2l/__init__.py
@@ -6,8 +6,3 @@
 #
 # Authors:
 #    Cédric Santran <santrancedric@gmail.com>
-
-# registering terminals and nonterminals with decorator in grammar (see grammar.py),
-# but without polluting the namespace
-# from .parser import terminal as __
-# from .parser import nonterminal as __
